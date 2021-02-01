4cd7b1a5e1cdf8f6aa5adf8791f49e7d1d48e13e
Gbadamson <gbadamosi.abubakar.95@gmail.com>
2021-02-01 10:55:56 +0100

    This is a plain react package for development

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BackComp from './Component/BackComp';
+
+function App() {
+  return (
+    <div>
+      <BackComp/>
+    </div>
+  );
+}
+
+export default App;
